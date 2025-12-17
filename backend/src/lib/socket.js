82d764442cffa0abb862cebd0d6d4f3bddff20dd
Ankit kumar <ankitsingh658231@gmail.com>
2025-12-17 13:38:41 +0530

    done with socket.io

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,52 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import { ENV } from "./env.js";
+import { socketAuthMiddleware } from "../middleware/socketAuthMiddleware.js";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: [ENV.CLIENT_URL],
+    credentials: true,
+  },
+});
+
+const userSocketMap = new Map();
+
+
+io.use(socketAuthMiddleware);
+
+
+io.on("connection", (socket) => {
+  const user = socket.user; 
+  const userId = user._id.toString();
+
+  console.log("User connected:", user.fullName);
+
+ 
+  userSocketMap.set(userId, socket.id);
+
+  // Join personal room (future-proof)
+  socket.join(userId);
+
+  // Send online users list to everyone
+  io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", user.fullName);
+
+    userSocketMap.delete(userId);
+
+    io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
+  });
+});
+
+
+export const getReceiverSocketId = (userId) => {
+  return userSocketMap.get(userId.toString());
+};
+
+export { io, app, server };
